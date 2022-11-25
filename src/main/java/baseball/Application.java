cdbd1afe72c3f2b8c48b894567ec7f40c6b9cd19
Irang <irang0426@gmail.com>
2022-11-25 14:49:35 +0900

    feat: add code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,39 +1,18 @@
 package baseball;
 
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
-        /*
-        - 기능 목록
-
-        Validation
-        입력과 정답 유효성 검사 (1부터 9로 이루어진 서로 다른 숫자 3개)
-
-        User
-        숫자 3개 입력
-
-        Computer
-        정답인 숫자 3개 랜덤으로 설정
-
-        Judgment
-        숫자 비교 후 결과 값 반환
-        정답일 경우 재시작/종료
-
-        View
-        재시작 여부 출력
-        결과 출력
-
-         */
-
         Computer computer = new Computer();
         System.out.println(computer.getRandom());
 
         Player player = new Player();
-        player.setNum();
-        System.out.println(player.getNum());
 
+        try {
+            player.input();
+        } catch (Exception e) {
+            System.out.println("유효하지 않는 입력 값입니다.");
+        }
+
+        System.out.println(player.getNumList());
     }
 }
--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,18 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
-    private int num;
+    private List<Integer> numList = new ArrayList<>();
 
-    public void setNum() {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        this.num = num;
+    public void input() {
+        List<Integer> num = new ArrayList<>();
+
+        System.out.print("세자리 숫자를 입력해주세요: ");
+        String str = Console.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            num.add(str.charAt(i) - '0');
+        }
+
+        if (isValidation(num)) {
+            this.numList = num;
+        } else {
+            throw new IllegalArgumentException();
+        }
+
     }
 
-    public int getNum() {
-        return num;
+    public List<Integer> getNumList() {
+        return this.numList;
+    }
+
+    private boolean isValidation(List<Integer> list) {
+        if (list.size() != 3) {
+            return false;
+        }
+
+        return true;
     }
 }
